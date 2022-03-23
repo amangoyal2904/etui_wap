a00e600dcade38233a1e7ba176c4655da9dd1be9
Barun Kumar <barun.kumar@timesinternet.in>
2022-03-23 11:58:03 +0530

    social share design done

--- src/components/SocialShare/index.tsx
+++ src/components/SocialShare/index.tsx
@@ -1,24 +1,18 @@
-import React from 'react'
-import styles from './styles.module.scss'
-// import SocialShare from "../../Utils/SocialShare";
+import styles from './styles.module.scss';
 
-
-export default function Share() {
-	let path = (typeof window !== 'undefined') ? window.location.pathname : '';
-	let shareParam = {
-		title: "",
-		url: 'https://m.economictimes.com' + path
-	}
-	return (
-			<div className={styles.wContainer}>
-				<p className={styles.wTitle}>Spread the word</p>
-				<p className={styles.wSubtitle}>If you like this page, share it on:</p>
-				{/* <div className={styles.iconContainer}>
-					<img src="/__assets/images/fb.png" onClick={e => SocialShare.Share(e, { ...shareParam, type: "fb" })} />
-					<img src="/__assets/images/twitter.png" onClick={e => SocialShare.Share(e, { ...shareParam, type: "twt" })} />
-					<img src="/__assets/images/linkedin.png" onClick={e => SocialShare.Share(e, { ...shareParam, type: "lin" })} />
-					<img src="/__assets/images/whatsapp_big.png" onClick={e => SocialShare.Share(e, { ...shareParam, type: "wa" })} />
-				</div> */}
-			</div>
-	)
+export default function SocialShare() {
+  return (
+    <div className={styles.socialShare}>
+      <div className={styles.shareText}>Share this Video</div>
+      <div className={styles.socialLinks}>
+        <span className={styles.wa}></span>
+        <span className={styles.fb}></span>
+        <span className={styles.twt}></span>
+        <span className={styles.in}></span>
+        <span className={styles.email}></span>
+        <span className={styles.sms}></span>
+        <span className={styles.bookmark}></span>        
+      </div>
+    </div>
+  )
 }
