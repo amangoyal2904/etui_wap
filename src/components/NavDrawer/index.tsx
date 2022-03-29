ececf6de17288a4b403be582b52ab2e3d9fb3d56
Abhishek <abhishek.banga@timesinternet.in>
2022-03-29 16:30:31 +0530

    fix for formatting nav drawer

--- src/components/NavDrawer/index.tsx
+++ src/components/NavDrawer/index.tsx
@@ -23,123 +23,69 @@ const NavDrawer: FC<DrawerProps> = ({ setIsDrawerOpen, isOpen }) => {
       if (ref.current && !ref.current.contains(e.target)) {
         setIsDrawerOpen(false);
       }
-    };
+    }
 
     document.addEventListener('mousedown', checkIfClickedOutside);
 
     return () => {
       //clean up
       document.removeEventListener('mousedown', checkIfClickedOutside);
-    };
+    }
   }, []);
+
+  const handleClick = (e) => {
+    // e.preventDefault();
+    setIsDrawerOpen(false);
+  };
 
   const showMore = (i: string) => {
     setIsSiblingsOpen((prev) => {
       return { ...prev, [i]: true };
     });
-  };
+  }
 
   const showSubmenu = (i: string) => {
     setIsSubmenuOpen((prev) => {
       return { ...prev, [i]: !prev[i] };
     });
   }
-  const handleClick = (e) => {
-    // e.preventDefault();
-    setIsDrawerOpen(false);
-  };
 
   const getMenu = (data: MenuProps, level: number, index: number | string) => {
     const r = data.sec.map((item, i) => {
-      return (
-        <React.Fragment key={level + '_' + i}>
-          <li
-            className={
-              level === 1 && i > 3 && !isSiblingsOpen[level + '_' + index]
-                ? styles.hidden
-                : ''
-            }
-          >
-            {makeLink(item, level, index, i)}
-            {level === 1 && item.sec && (
-              <span
-                className={
-                  !isSubmenuOpen[level + '_' + index + '_' + i]
-                    ? styles.rDown
-                    : styles.rUp
-                }
-                onClick={() => showSubmenu(level + '_' + index + '_' + i)}
-              ></span>
-            )}
-            {item.sec && getMenu(item, level + 1, index + '_' + i)}
-          </li>
-          {level === 1 && i === 4 && !isSiblingsOpen[level + '_' + index] && (
-            <li
-              data-more={item.title}
-              onClick={() => showMore(level + '_' + index)}
-            >
-              VIEW MORE FROM {data.title} +
-            </li>
-          )}
-          {level === 0 && <li className={styles.oneDotBdr}></li>}
-        </React.Fragment>
-      );
+      console.log('getMenu::', item);
+      return <React.Fragment key={level + '_' + i}>
+        <li className={(level === 1 && i > 3 && !isSiblingsOpen[level + '_' + index]) ? styles.hidden : ''}>
+          {makeLink(item, level, index, i)}
+          {level === 1 && item.sec && <span className={!isSubmenuOpen[level + '_' + index + '_' + i] ? styles.rDown : styles.rUp} onClick={() => showSubmenu(level + '_' + index + '_' + i)}></span>}
+          {item.sec && getMenu(item, level + 1, index + '_' + i)}
+        </li>
+        {(level === 1 && i === 4 && !isSiblingsOpen[level + '_' + index]) && <li data-more={item.title} onClick={() => showMore(level + '_' + index)}>VIEW MORE FROM {data.title} +</li>}
+        {level === 0 && <li className={styles.oneDotBdr}></li>}
+      </React.Fragment>
     });
 
-    return level !== 0 ? (
-      <ul
-        className={
-          level === 2 && !isSubmenuOpen[level - 1 + '_' + index]
-            ? styles.hidden
-            : styles.submenu
-        }
-      >
-        {r}
-      </ul>
-    ) : (
-      r
-    );
-  };
+    return level !== 0 ? <ul className={level === 2 && !isSubmenuOpen[(level - 1) + '_' + index] ? styles.hidden : styles.submenu}>{r}</ul> : r;
+  }
 
-  const makeLink = (
-    data: MenuProps,
-    level: number,
-    iOuter: number | string,
-    iInner: number | string
-  ) => {
+  const makeLink = (data: MenuProps, level: number, iOuter: number | string, iInner: number | string) => {
     if (!(data.shorturl || data.url)) {
-      return (
-        <>
-          {data.title} {level === 0 && <span className={styles.rArr}></span>}
-        </>
-      );
+      return <>{data.title} {level === 0 && <span className={styles.rArr}></span>}</>;
     }
 
     return (
       <Link href={data.shorturl ? data.shorturl : data.url}>
-        <a
-          onClick={handleClick}
-          className={
-            isSubmenuOpen[level + '_' + iOuter + '_' + iInner]
-              ? styles.bold
-              : ''
-          }
-        >
+        <a onClick={handleClick} className={isSubmenuOpen[level + '_' + iOuter + '_' + iInner] ? styles.bold : ''}>
           {data.title} {level === 0 && <span className={styles.rArr}></span>}
         </a>
       </Link>
     );
-  };
+  }
 
-  return menuData ? (
-    <nav
-      className={`${styles.drawer} ${isOpen ? styles.isOpen : ''}`}
-      ref={ref}
-    >
+  return (
+    menuData ? <nav className={`${styles.drawer} ${isOpen ? styles.isOpen : ''}`} ref={ref}>
       <div className={styles.user}>
         <div className={styles.userName}>
-          <div>Welcome</div>
-          <div>User</div>
+          <div>Welcome</div><div>User</div>
         </div>
         <div className={styles.signIn}>
           <div className={styles.userIcon}></div>
@@ -157,8 +103,8 @@ const NavDrawer: FC<DrawerProps> = ({ setIsDrawerOpen, isOpen }) => {
           {getMenu(menuData, 0, 0)}
         </ul>
       </div>
-    </nav>
-  ) : null;
-};
+    </nav> : null
+  );
+}
 
 export default NavDrawer;
