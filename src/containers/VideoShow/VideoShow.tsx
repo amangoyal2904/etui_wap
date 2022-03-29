5938c2d1257ab4287e29905607edeac7d5044770
rohitazadtil <rohit.azad@timesinternet.in>
2022-03-29 13:06:33 +0530

    api node change

--- src/containers/VideoShow/VideoShow.tsx
+++ src/containers/VideoShow/VideoShow.tsx
@@ -9,11 +9,11 @@ interface PageProps {
 }
 
 const VideoShow: NextPage<PageProps> = ({ query, data }) => {
-  const _seoOrgImgUrl = data.searchResult[3].data.seo;
-  let seoData = data.searchResult[2].data;
+  const _seoOrgImgUrl = data.common_config.seo;
+  let seoData = data.seo;
       seoData.seoOrgImgUrl = _seoOrgImgUrl;
       
-  console.log('data', data.searchResult[2].data)
+  //console.log('data', data.searchResult[2].data)
   return (
     <div className={styles.header}>
       <h1>Video Show</h1>
