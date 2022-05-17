2cfd3507dcc114e6c499b93d4e65d11c6e6682a3
Barun Kumar <barun.kumar@timesinternet.in>
2022-05-17 23:47:36 +0530

    page transition top progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,21 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { callJsOnRouteChange, InitialJsOnAppLoad } from "../utils/priority";
 import { wrapper } from "../app/store";
+import ProgressBar from "components/PageTransition";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#209cee",
+  className: "bar-of-progress",
+  delay: 10
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 declare global {
   interface Window {
     initalJsCalled: boolean;
