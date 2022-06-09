6915c999e8978195d62446ed86f161ed966f3177
Abhishek <abhishek.banga@timesinternet.in>
2022-06-09 17:55:02 +0530

    app download device check option

--- src/components/AppDownloadWidget/index.tsx
+++ src/components/AppDownloadWidget/index.tsx
@@ -1,32 +1,50 @@
 import React from "react";
+import { getMobileOS } from "utils";
+import { appLinks } from "utils/common";
 import styles from "./styles.module.scss";
 
 interface WidgetData {
   tpName: string;
 }
 const AppDownloadWidget = (props: WidgetData) => {
+  const handleRedirect = () => {
+    let CTA_url = "";
+    const os = getMobileOS();
+    if (os === "Android") {
+      CTA_url = appLinks["android"] + "&utm_source=pwa_widget_homepage&utm_campaign=pwa_widget_homepage";
+    } else if (os === "iOS") {
+      CTA_url = appLinks["ios"];
+    } else {
+      CTA_url = appLinks["generic"] + "?utm_source=pwa_widget_homepage&utm_medium=pwa_widget_homepage";
+    }
+    window.location.href = CTA_url;
+  };
+
   const { tpName } = props;
   return (
     <>
-      <div className={styles.topWrap}>
-        <a href="https://uj2g8.app.goo.gl/?link=https://economictimes.indiatimes.com&amp;apn=com.et.reader.activitiesisi=474766725&amp;ibi=com.til.ETiphone&amp;efr=1">
-          <div className={styles.wrap}>
-            <div className={styles.innerWrap}>
-              <p className={styles.innerHead}>
-                Enjoy <b>seamless experience</b>
-                <br />
-                on our App!
-              </p>
-              <p className={styles.innerText}>
-                <span className={styles.leftTick}></span>We’re 10X faster on app
-              </p>
-              <p className={styles.innerText}>
-                <span className={styles.leftTick}></span>Better viewing experience
-              </p>
-              <p className={styles.installBtn}>INSTALL ET APP</p>
-            </div>
+      <div
+        onClick={() => {
+          handleRedirect();
+        }}
+        className={styles.topWrap}
+      >
+        <div className={styles.wrap}>
+          <div className={styles.innerWrap}>
+            <p className={styles.innerHead}>
+              Enjoy <b>seamless experience</b>
+              <br />
+              on our App!
+            </p>
+            <p className={styles.innerText}>
+              <span className={styles.leftTick}></span>We’re 10X faster on app
+            </p>
+            <p className={styles.innerText}>
+              <span className={styles.leftTick}></span>Better viewing experience
+            </p>
+            <p className={styles.installBtn}>INSTALL ET APP</p>
           </div>
-        </a>
+        </div>
       </div>
     </>
   );
