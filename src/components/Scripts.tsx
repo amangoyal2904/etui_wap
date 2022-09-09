203c26d1e52c655487da9673287662668350ec51
Barun Kumar <barun.kumar@timesinternet.in>
2022-09-09 10:08:56 +0530

    script condtion removed

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -48,6 +48,50 @@ const Scripts: FC<Props> = ({ isprimeuser, objVc }) => {
         }}
       />
 
+      <Script
+        id="google-analytics"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', '${Config.GA.GA_ID}', 'auto');
+          window.customDimension = { ...window["customDimension"], page: window.location.href };
+          ga('send', 'pageview', window.customDimension);
+          const gaLoaded = new Event('gaLoaded');
+          document.dispatchEvent(gaLoaded);
+          `
+        }}
+      />
+      <Script
+        src="https://static.growthrx.in/js/v2/web-sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          window.grx("init", window.objVc.growthRxId || "gc2744074");
+          window.customDimension = { ...window["customDimension"], url: window.location.href };
+          window.grx("track", "page_view", window.customDimension);
+        }}
+      />
+      <Script
+        id="tag-manager"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${Config.GA.GTM_KEY}`}
+      />
+      <Script
+        id="tag-manager-init"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', '${Config.GA.GTM_ID}', { page_path: window.location.pathname });
+          `
+        }}
+      />
+
       {!reqData.opt && !isprimeuser && isReady && (
         <>
           <Script
@@ -61,49 +105,6 @@ const Scripts: FC<Props> = ({ isprimeuser, objVc }) => {
           {router.asPath.indexOf("skip_ctn=1") == -1 && (
             <Script src="https://static.clmbtech.com/ad/commons/js/2501/colombia_v2.js" strategy="lazyOnload" />
           )}
-          <Script
-            id="google-analytics"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-              ga('create', '${Config.GA.GA_ID}', 'auto');
-              window.customDimension = { ...window["customDimension"], page: window.location.href };
-              ga('send', 'pageview', window.customDimension);
-              const gaLoaded = new Event('gaLoaded');
-              document.dispatchEvent(gaLoaded);
-              `
-            }}
-          />
-          <Script
-            src="https://static.growthrx.in/js/v2/web-sdk.js"
-            strategy="lazyOnload"
-            onLoad={() => {
-              window.grx("init", window.objVc.growthRxId || "gc2744074");
-              window.customDimension = { ...window["customDimension"], url: window.location.href };
-              window.grx("track", "page_view", window.customDimension);
-            }}
-          />
-          <Script
-            id="tag-manager"
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${Config.GA.GTM_KEY}`}
-          />
-          <Script
-            id="tag-manager-init"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() { dataLayer.push(arguments); }
-                gtag('js', new Date());
-                gtag('config', '${Config.GA.GTM_ID}', { page_path: window.location.pathname });
-              `
-            }}
-          />
         </>
       )}
     </>
