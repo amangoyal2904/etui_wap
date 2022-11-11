31b1cff8eaf428ca804e332596891b83dbf2e853
Dayanidhi Gupta <dayanidhi.gupta@timesinternet.in>
2022-11-11 15:51:33 +0530

    handled bad request for videoshow

--- src/pages/[[...all]].tsx
+++ src/pages/[[...all]].tsx
@@ -46,7 +46,7 @@ export const getServerSideProps = wrapper.getServerSideProps((store) => async ({
   const data = response?.[page]?.data || {};
   if (
     (data?.searchResult?.[0].data?.responseStatus && data?.searchResult?.[0].data?.responseStatus != 200) ||
-    data?.error?.code >= 400
+    data?.error
   ) {
     page = "notFound";
     await store.dispatch(setCommonData({ page: "notFound", data: {} }));
