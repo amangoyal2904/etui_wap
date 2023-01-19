d8246fc6b482e3b646c6869cbff4e8bf89041cf0
Maneet Rana <maneet.rana@timesinternet.in>
2023-01-19 12:13:03 +0530

    cube changes done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import DynamicFooter from "components/DynamicFooter";
 import DfpAds from "components/Ad/DfpAds";
 import Cube from "components/Cube";
 
 const Footer: FC<{ dynamicFooterData: any }> = ({ dynamicFooterData }) => {
+  const [isCubeActive, setIsCubeActive] = useState(false);
+
+  useEffect(() => {
+    const objVc = window.objVc || {};
+    if (objVc.global_cube_wap && objVc.global_cube_wap == 1) setIsCubeActive(true);
+  }, []);
+
   return (
     <>
       <footer id="wapFooter" className={styles.wapFooter}>
         <DynamicFooter dynamicFooterData={dynamicFooterData} />
         <DfpAds adInfo={{ key: "andbeyond" }} />
-        <Cube />
+        {isCubeActive && <Cube />}
       </footer>
     </>
   );
